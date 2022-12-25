062997b2d9386a2248ff2970400b5cd93870e686
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2022-12-25 21:30:21 +0200

    deals2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-// import Swiper JS
 import Swiper from 'swiper/swiper-bundle';
-// // import Swiper styles
-// import 'swiper/css';
-
-// import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'swiper/swiper-bundle.min.css';
 
 const swiper = new Swiper('.slider-main-block', {
@@ -15,4 +10,27 @@ const swiper = new Swiper('.slider-main-block', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+// Таби
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener('click', function (e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  if (targetElement.closest('.tabs-deals__button')) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains('active')) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove('active');
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+    targetElement.classList.add('active');
+    tabItems[currentActiveIndex].classList.remove('active');
+    tabItems[newActiveIndex].classList.add('active');
+  }
 });
